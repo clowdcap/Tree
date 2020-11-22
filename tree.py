8ad7825607dea92ab24296681ae97445bf17bfcb
Jose Marinho <clowdcap@hotmail.com>
2020-11-21 23:09:43 -0300

    Update tree.py
    Melhorias nas buscas de dados na arvore

--- tree.py
+++ tree.py
@@ -9,16 +9,96 @@ class Node:
 
 
 class BinaryTree:
-    def __init__(self, data):
-        node = Node(data)
-        self.root = node
+    def __init__(self, data=None):
+        if data:
+            node = Node(data)
+            self.root = node
+        else:
+            self.root = None
+
+    # Percurso em ordem simetrica ( Travessia_simétrica() )
+    def simetric_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            print('(', end='')
+            self.simetric_traversal(node.left)
+        print(node, end='')
+        if node.right:
+            self.simetric_traversal(node.right)
+            print(')', end='')
+
+    def postorder_traversal(self, node=None):
+        if node is None:
+            node = self.root
+        if node.left:
+            self.postorder_traversal(node.left)
+        if node.right:
+            self.postorder_traversal(node.right)
+        print(node)
+
+
+def postorder_exemple_tree():
+    tree = BinaryTree()
+    n1 = Node('I')
+    n2 = Node('N')
+    n3 = Node('S')
+    n4 = Node('C')
+    n5 = Node('R')
+    n6 = Node('E')
+    n7 = Node('V')
+    n8 = Node('A')
+    n9 = Node('5')
+    n0 = Node('3')
+
+    n0.left = n6
+    n0.right = n9
+    n6.left = n1
+    n6.right = n5
+    n5.left = n2
+    n5.right = n4
+    n4.right = n3
+    n9.left = n8
+    n8.right = n7
+
+    tree.root = n0
+    return tree
 
 
 if __name__ == "__main__":
-    tree = BinaryTree('RAIZ')
-    tree.root.left = Node('ESQUERDA')
-    tree.root.right = Node('DIREITA')
+    tree = postorder_exemple_tree()
+    print("Percurso em pós ordem:")
+    tree.postorder_traversal()
 
-    print(tree.root)
-    print(tree.root.left)
-    print(tree.root.right)
+""" if __name__ == "__main__":
+    tree = BinaryTree()
+    n1 = Node('a')
+    n2 = Node('+')
+    n3 = Node('*')
+    n4 = Node('b')
+    n5 = Node('-')
+    n6 = Node('/')
+    n7 = Node('c')
+    n8 = Node('d')
+    n9 = Node('e')
+
+    n6.left = n7
+    n6.right = n8
+    n5.left = n6
+    n5.right = n9
+    n3.left = n4
+    n4.right = n5
+    n2.left = n1
+    n2.right = n3
+
+    tree.root = n2
+    tree.simetric_traversal()
+    print('')
+
+tree = BinaryTree('RAIZ')
+tree.root.left = Node('ESQUERDA')
+tree.root.right = Node('DIREITA')
+
+print(tree.root)
+print(tree.root.left)
+print(tree.root.right)"""
