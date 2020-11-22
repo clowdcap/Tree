47399067842b3b6bc2f0cc1e7ea06a78b40115df
Jose Marinho <clowdcap@hotmail.com>
2020-11-22 15:13:22 -0300

    Update tree.py
    Erro encontrado:
    - Ao puxar o def levelorder_traversal(), dentro ha uma função da fila, que é o def pop(), nele esta indefinido : self.primeiro = self.primeiro. ** data/left/right ** <-- Ver qual é o certo

--- tree.py
+++ tree.py
@@ -192,11 +192,11 @@ if __name__ == "__main__":
         return tree
 
 
-    bst = example_tree()
-    bst.inorder_traversal()
-    print('\n-----')
-    bst.levelorder_traversal()
-
+#    bst = example_tree()
+#    bst.inorder_traversal()
+#    print('\n-----')
+#    bst.levelorder_traversal()
+#
 # SISTEMA DE BUSCA
 # print('\n-----')
 # items = [30, 33, 55, 61, 117]
@@ -207,100 +207,3 @@ if __name__ == "__main__":
 #        print('Elemento', item, 'não encontrado')
 #    else:
 #        print('Elemento', r.root.data, 'foi encontrado')
-
-""" if __name__ == "__main__":
-    tree = BinaryTree()
-    n1 = Node('a')
-    n2 = Node('+')
-    n3 = Node('*')
-    n4 = Node('b')
-    n5 = Node('-')
-    n6 = Node('/')
-    n7 = Node('c')
-    n8 = Node('d')
-    n9 = Node('e')
-
-    n6.left = n7
-    n6.right = n8
-    n5.left = n6
-    n5.right = n9
-    n3.left = n4
-    n4.right = n5
-    n2.left = n1
-    n2.right = n3
-
-    tree.root = n2
-    tree.simetric_traversal()
-    print('')
-
-tree = BinaryTree('RAIZ')
-tree.root.left = Node('ESQUERDA')
-tree.root.right = Node('DIREITA')
-
-print(tree.root)
-print(tree.root.left)
-print(tree.root.right)
-
-# exemplo 2
-if __name__ == "__main__":
-    tree = postorder_exemple_tree()
-    print("Percurso em pós ordem:")
-    tree.postorder_traversal()
-    print('Altura: ', tree.height())"""
-
-'''
-def search(self, elemento, node=0):
-    if node == 0:
-        node = self.root
-    if node is None or node.data == elemento:
-        return BinarySearchTree(node)
-    if elemento < node.data:
-        return self.search(elemento, node.left)
-    return self.search((elemento, node.right))
-
-
-def postorder_exemple_tree():
-    tree = BinaryTree()
-    n1 = Node('I')
-    n2 = Node('N')
-    n3 = Node('S')
-    n4 = Node('C')
-    n5 = Node('R')
-    n6 = Node('E')
-    n7 = Node('V')
-    n8 = Node('A')
-    n9 = Node('5')
-    n0 = Node('3')
-
-    n0.left = n6
-    n0.right = n9
-    n6.left = n1
-    n6.right = n5
-    n5.left = n2
-    n5.right = n4
-    n4.right = n3
-    n9.left = n8
-    n8.right = n7
-
-    tree.root = n0
-    return tree
-
-
-    if __name__ == "__main__":
-    random.seed(40)
-
-    elemento = random.sample(range(1, 1000), 38)
-
-    bst = BinarySearchTree()
-    for v in elemento:
-        bst.insert(v)
-
-    bst.inorder_traversal()
-
-    items = [30, 33, 55, 61, 117]
-    for item in items:
-        r = bst.search(item)
-        if r is None:
-            print('Elemento', item, 'não encontrado')
-        else:
-            print('Elemento', r.root.data, 'foi encontrado')'''
